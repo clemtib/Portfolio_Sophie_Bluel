d491b7987c18688a1b70681077d3b0675ea7d842
Clement TIBERGHIEN <clement.tiberghien.pro@gmail.com>
2023-01-16 17:37:30 +0100

    sessionStorage OK

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -12,23 +12,54 @@ formLogin.addEventListener('submit', function(event){
     }
     const chargeUtile = JSON.stringify(login)
     fetch("http://localhost:5678/api/users/login", {
+       
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
         body: chargeUtile
     })       
-        .then(function (reponse) {
+        .then(async function (reponse) {
             if (reponse.ok) {
-                // document.location.href="index.html"
-                console.log(chargeUtile)
-                console.log(reponse)
+                reponse = await reponse.json()
+                sessionStorage.setItem('adminId', reponse.token)
+                document.location.href = "index.html"
+                
+                console.log(sessionStorage.getItem('adminId'))
+            
             } else {
                 window.alert("identification ou mot de passe eronné")
+                
              
         }
         })
         .catch(function (error) {
         console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message)
         })
+   
 })
+//   .then(function (reponse) {
+//             if (reponse.ok) {
+//                 // document.location.href="index.html"
+//                 reponse.json()
+//                     .then(function (reponse) {
+//                         sessionStorage.setItem('userId', token)
+//                     console.log(reponse.token)
+//                 })
+            
+           
+//             } else {
+//                 window.alert("identification ou mot de passe eronné")
+             
+//         }
+//         })
+//         .catch(function (error) {
+//         console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message)
+//         })
+   
 
+   
+               
+ 
 
